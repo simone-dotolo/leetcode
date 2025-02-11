fbc6d3768315b699a1e49943cc58f6b8c9e517bf
simone-dotolo <84937474+simone-dotolo@users.noreply.github.com>
2025-02-11 11:08:41 +0100

    1910

---
+++ Medium/1910.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        for char in s:
+            stack.append(char)
+            if len(stack) >= len(part) and ''.join(stack[-len(part):]) == part:
+                for _ in range(len(part)):
+                    stack.pop()
+        return ''.join(stack)

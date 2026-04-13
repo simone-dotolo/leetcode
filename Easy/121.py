2d555b062eed09bda90bd0caaff1e4e1b84926cf
simone-dotolo <84937474+simone-dotolo@users.noreply.github.com>
2026-04-13 20:33:04 +0200

    121

---
+++ Easy/121.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        res = 0
+        for price in prices:
+            res = max(res, price - buy)
+            buy = min(buy, price)
+        return res
